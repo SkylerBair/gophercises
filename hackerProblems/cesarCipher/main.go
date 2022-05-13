9536b76a114b7079a4d26185747f7002398ab71d
skyler bair <skylertimothybair@hotmail.com>
2022-05-12 22:57:32 -0600

    added alternate solutions usng more niche case

--- hackerProblems/cesarCipher/main.go
+++ hackerProblems/cesarCipher/main.go
@@ -12,48 +12,58 @@ func main() {
 	fmt.Scanf("%s\n", &input)
 	fmt.Scanf("%d\n", &delta)
 
-	fmt.Println("length: %d\n", length)
-	fmt.Println("input: %s\n", input)
-	fmt.Println("delta: %d\n", delta)
-
-	alphabetLower := "abcdefghijklmnopqrstuvwxyz"
-	alphabetUpper := "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-	ret := ""
+	var ret []rune
 	for _, ch := range input {
-		switch {
-		case strings.IndexRune(alphabetLower, ch) >= 0:
-			ret = ret + string(rotate(ch, delta, []rune(alphabetLower)))
-		case strings.IndexRune(alphabetUpper, ch) >= 0:
-			ret = ret + string(rotate(ch, delta, []rune(alphabetUpper)))
-		default:
-			ret = ret + string(ch)
-		}
+		ret = append(ret, cipher(ch, delta))
 	}
-	fmt.Println(ret)
+	fmt.Println(string(ret))
 }
 
-func rotate(s rune, delta int, key []rune) rune {
-	idx := strings.IndexRune(string(key), s)
-	if idx < 0 {
-		panic("idx < 0")
+func cipher(r rune, delta int) rune {
+	if r >= 'A' && r <= 'Z' {
+		return rotate(r, 'A', delta)
 	}
-	idx = (idx + delta) % len(key)
-	return key[idx]
+	if r >= 'a' && r <= 'z' {
+		return rotate(r, 'a', delta)
+	return r
 }
 
-// func caesarCipher(s string, k []byte) string {
-// 	// Write your code here
-// 	abc := [26]string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
-// 	trimmed := strings.TrimSpace(s)
-// 	buf := []byte("")
-// 	for letterIndex, letter := range trimmed {
-// 		for abcIndex, abcLetter := range abc {
-// 			if letter == abcLetter {
-// 				encrypted := abc[abcIndex+k]
-// 				buf = append(buf, []byte(encrypted))
-// 			}
+func rotate(r rune, base, delta int) rune {
+	tmp := int(r) - base 
+	tmp = (tmp + delta) % 26
+	return rune(tmp + base)
+}
+
+// func main() {
+// 	var length, delta int
+// 	var input string
+// 	fmt.Scanf("%d\n", &length)
+// 	fmt.Scanf("%s\n", &input)
+// 	fmt.Scanf("%d\n", &delta)
+
+// 	alphabetLower := "abcdefghijklmnopqrstuvwxyz"
+// 	alphabetUpper := "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+// 	ret := ""
+// 	for _, ch := range input {
+// 		switch {
+// 		case strings.IndexRune(alphabetLower, ch) >= 0:
+// 			ret = ret + string(rotate(ch, delta, []rune(alphabetLower)))
+// 		case strings.IndexRune(alphabetUpper, ch) >= 0:
+// 			ret = ret + string(rotate(ch, delta, []rune(alphabetUpper)))
+// 		default:
+// 			ret = ret + string(ch)
 // 		}
-
 // 	}
-// 	return trimmed
+// 	fmt.Println(ret)
 // }
+
+// func rotate(s rune, delta int, key []rune) rune {
+// 	idx := strings.IndexRune(string(key), s)
+// 	if idx < 0 {
+// 		panic("idx < 0")
+// 	}
+// 	idx = (idx + delta) % len(key)
+// 	return key[idx]
+// }
+
+
